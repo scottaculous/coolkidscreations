508f57b422058e3ebdd27dc398dd1483dfe60b86
unknown <scottaculous@gmail.com>
2021-12-05 15:43:55 -0500

    initial commit

---
+++ src/pages/page-2.js
@@ -0,0 +1,29 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <Seo title="Page two" />
+    <h1>Hi</h1>
+    <p>We are a group of kids that make cool stuff.  We founded in August 2021.  I hope you like our site.</p>
+    <p>Thanks, 
+       gamer &amp; cool
+    </p>
+    <StaticImage
+      src="../images/cool_gamer.jpg"
+      width={400}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <br />
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default SecondPage
