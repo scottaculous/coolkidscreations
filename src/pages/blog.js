feea60c2ef23bb50c18379d02491312eb9b4e019
unknown <scottaculous@gmail.com>
2021-12-21 22:57:22 -0500

    Updated blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -10,13 +10,16 @@ const Blog = () => (
     <h1>Blog</h1>
     
     <ol>
-      <li>Bought a domain through Amazon Web Services (AWS) - coolkidscreations</li>
-      <li>Linked domain to a S3 bucket</li>
-      <li>Dwonloaded a Gatsby template from gatsbyjs.com on GitHub using git clone</li>
+      <li>Bought a domain through Route53 on Amazon Web Services (AWS) - coolkidscreations.com</li>
+      <li>Created a S3 bucket and linked domain to the bucket</li>
+      <li>Downloaded a Gatsby template from gatsbyjs.com on GitHub using git clone</li>
       <li>Modified the template to coolkidscreations</li>
       <li>Setup coolkidscreations repo in GitHub</li>
+      <li>Built the website using <code>npm run build</code></li>
+      <li>Deployed the website using <code>npm run deploy</code></li>
       <li>Made a logo.  Fun fact the logo means roblox and minecraft.</li>
-    </ol>
+      <li>Uploaded our first product</li>
+    </ol>   
 
   </Layout>
 )
