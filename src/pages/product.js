e65afd6572f276bd0c2d4ca76ed59ab020642d5a
unknown <scottaculous@gmail.com>
2022-05-03 17:13:06 -0400

    Edited 404 not found page

--- src/pages/product.js
+++ src/pages/product.js
@@ -60,7 +60,7 @@ const Product = () => (
       </tr>
     </table>
 
-    <h3>Xmas Time - $9.99</h3>
+    <h3>Xmas Time - SOLD OUT</h3>
     <StaticImage
       src="../images/product-3.jpeg"
       width={500}
@@ -69,22 +69,8 @@ const Product = () => (
       alt="Product 3"
       style={{ marginBottom: `1.45rem` }}
     />
-     <table border="0">
-      <tr>
-        <td width="100">
-    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-    <input type="hidden" name="cmd" value="_s-xclick" />
-    <input type="hidden" name="hosted_button_id" value="V4B2KQJYRBFAU" />
-    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
-    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-    </form>
-            </td>
-            <td align="left" valign="top">
-            or  <Link to="/payment/">Pay with bitcoin</Link>
-            </td>
-          </tr>
-        </table>
 
+    <br/>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
